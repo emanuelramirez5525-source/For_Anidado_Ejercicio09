5cf16ec79518105b6904166ed5199a20e611cd6b
emanu <emanu@EMANUEL_RAMREZ>
2026-02-28 15:35:11 -0600

    Logica aplicada!

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 ﻿// Mostrar un patrón donde cada fila tenga el número de la fila repetido.
+int filas = 5;
 
+for (int fila = 1; fila <= filas; fila++)
+{
+    for (int j = 1; j <= fila; j++)
+    {
+        Console.Write(fila + "");
+    }
+    Console.WriteLine();
+}
